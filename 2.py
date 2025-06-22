d6e684d7a3f9a0c2c450fca0c6b6c3f6ff12e629
Nitheesha <nitheeshareddy01@gmail.com>
2025-06-22 21:07:30 +0530

    Day 2

---
+++ 2.py
@@ -0,0 +1,164 @@
+x = 5
+print(type(x))
+
+# str
+x = "Hello"
+print(x)
+print(type(x))
+
+# int
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+# float
+x = 20.10
+print(x)
+print(type(x))
+
+# complex
+x = complex(1j)
+print(x)
+print(type(x))
+
+# list
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+
+# tuple
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x))
+
+# range
+x = range(4)
+print(x)
+print(type(x))
+
+# dict
+x= {"name" : "nitheesha", "age" : "44 "}
+print(x)
+print(type(x))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# int
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# float
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# complex
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Convert from one type to another:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# type convertion
+# Integers:
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+# Floats:
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+
+# Strings:
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x)
+print(y)
+print(z)
+
+# either single quotation marks, or double quotation marks both are same
+print("Hello")
+print('Hello')
+
+# quotes inside a string
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+# assign variable to a string
+a = "Hello"
+print(a)
+
+# to get position of a character(first position starts with 0)
+a = "Nitheesha"
+print(a[1])
+
+# length of a string
+a = "Hello, World!"
+print(len(a))
+
+# To check if a certain character is present in a string
+txt = "My name is Nitheesha"
+print("Nitheesha" in txt)
+
+# Print only if "Nitheesha" is present:
+txt = "My name is Nitheesha"
+if "Nitheesha" in txt:
+  print("Yes, 'Nitheesha' is present.")
+
+
+
+
