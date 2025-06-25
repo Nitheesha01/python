2c03014101e8c91598000715a7821317150493e3
Nitheesha <nitheeshareddy01@gmail.com>
2025-06-25 22:35:10 +0530

    Day 2

--- 4.py
+++ 4.py
@@ -22,7 +22,7 @@ print(len(thistuple))
 thistuple = ("apple",)
 print(type(thistuple))
 
-#NOT a tuple
+# NOT a tuple
 thistuple = ("apple")
 print(type(thistuple))
 
@@ -66,7 +66,7 @@ print(thistuple[-4:-1])
 
 thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
+    print("Yes, 'apple' is in the fruits tuple")
 
 # we can not change tuple directly
 x = ("apple", "banana", "cherry")
@@ -97,7 +97,7 @@ thistuple = tuple(y)
 # to delete the tuple
 thistuple = ("apple", "banana", "cherry")
 del thistuple
-print(thistuple) #this will raise an error because the tuple no longer exists
+print(thistuple)  # this will raise an error because the tuple no longer exists
 
 fruits = ("apple", "banana", "cherry")
 (green, yellow, red) = fruits
@@ -121,21 +121,21 @@ print(red)
 # loop
 thistuple = ("apple", "banana", "cherry")
 for x in thistuple:
-  print(x)
+    print(x)
 
 thistuple = ("apple", "banana", "cherry")
 for i in range(len(thistuple)):
-  print(thistuple[i])
+    print(thistuple[i])
 
 # while loop
 thistuple = ("apple", "banana", "cherry")
 i = 0
 while i < len(thistuple):
-  print(thistuple[i])
-  i = i + 1
+    print(thistuple[i])
+    i = i + 1
 
 # to join two are more tuples
-tuple1 = ("a", "b" , "c")
+tuple1 = ("a", "b", "c")
 tuple2 = (1, 2, 3)
 tuple3 = tuple1 + tuple2
 print(tuple3)
@@ -144,8 +144,6 @@ print(tuple3)
 fruits = ("apple", "banana", "cherry")
 mytuple = fruits * 2
 print(mytuple)
-
-
 
 # set
 # to create a set
@@ -179,7 +177,7 @@ print(type(myset))
 # loop a set
 thisset = {"apple", "banana", "cherry"}
 for x in thisset:
-  print(x)
+    print(x)
 
 thisset = {"apple", "banana", "cherry"}
 print("banana" in thisset)
@@ -223,7 +221,6 @@ set2 = {1, 2, 3}
 set3 = set1.union(set2)
 print(set3)
 
-
 # another method
 set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
@@ -243,7 +240,7 @@ set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
 set3 = {"John", "Elena"}
 set4 = {"apple", "bananas", "cherry"}
-myset = set1 | set2 | set3 |set4
+myset = set1 | set2 | set3 | set4
 print(myset)
 
 # Join a set with a tuple:
@@ -253,14 +250,329 @@ z = x.union(y)
 print(z)
 
 # The update() method inserts the items in set2 into set1:
-set1 = {"a", "b" , "c"}
+set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
 set1.update(set2)
 print(set1)
 
-
 # Join set1 and set2, that only contains the items that are present in both sets
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
+set3 = set1.intersection(set2)  # set3 = set1 & set2
+print(set3)
+
+# Keep the items that exist in both set1, and set2 keep in set1
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.intersection_update(set2)
+print(set1)
+
+# Join sets that contains the values True, False, 1, and 0, and see what is considered as duplicates:
+set1 = {"apple", 1, "banana", 0, "cherry"}
+set2 = {False, "google", 1, "apple", 2, True}
 set3 = set1.intersection(set2)
 print(set3)
+
+# The difference() method will return a new set that will contain only the items from the first set that are not present in the other set
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.difference(set2)
+print(set3)
+
+# Dictionary
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+# Print the "brand" value of the dictionary:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["brand"])
+
+# Duplicate values will overwrite existing values:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
+}
+print(thisdict)
+print(len(thisdict))
+
+# String, int, boolean, and list data types:
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
+
+# Print the data type of a dictionary:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(type(thisdict))
+
+# Get the value of the "model" key:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = thisdict["model"]  # x = thisdict.get("model")
+
+#  Add a new item to the original dictionary
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = car.keys()
+print(x)  # before the change
+car["color"] = "white"
+print(x)  # after the change
+x = thisdict.values()
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = car.values()
+print(x)  # before the change
+car["color"] = "red"
+print(x)  # after the change
+x = thisdict.items()
+
+# using if condition
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+# pop()
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+# using popitem()
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+# del
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+# clear()
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+# using for loop
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x in thisdict:
+    print(x)
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x in thisdict:
+    print(thisdict[x])
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+for x in thisdict.values():
+    print(x)
+
+# Make a copy of a dictionary with the copy() method:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+# Make a copy of a dictionary with the dict() function:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+# Create a dictionary that contain three dictionaries
+myfamily = {
+    "child1": {
+        "name": "Emil",
+        "year": 2004
+    },
+    "child2": {
+        "name": "Tobias",
+        "year": 2007
+    },
+    "child3": {
+        "name": "Linus",
+        "year": 2011
+    }
+}
+print(myfamily)
+
+# if statement
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+# elif
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+# else
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+# without elif
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# One line if statement:
+if a > b: print("a is greater than b")
+
+# One line if else statement:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# One line if else statement, with 3 conditions:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# Test if a is greater than b, AND if c is greater than a:
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
+
+# Test if a is greater than b, OR if a is greater than c:
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+# Test if a is NOT greater than b:
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+# nested if
+age = 25
+has_id = True
+if age >= 18:
+    print("You are an adult.")
+
+    if has_id:
+        print("You are allowed to enter.")
+    else:
+        print("You need to show ID to enter.")
+else:
+    print("You are not allowed to enter. Age must be 18 or above.")
+
+# pass ststement
+a = 33
+b = 200
+if b > a:
+  pass
+
+# match statement
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+
+# se the underscore character _ as the last case value if you want a code block to execute when there are not other matches:
+day = 4
+match day:
+  case 6:
+    print("Today is Saturday")
+  case 7:
+    print("Today is Sunday")
+  case _:
+    print("Looking forward to the Weekend")
+
+# combine
+day = 4
+match day:
+  case 1 | 2 | 3 | 4 | 5:
+    print("Today is a weekday")
+  case 6 | 7:
+    print("I love weekends!")
